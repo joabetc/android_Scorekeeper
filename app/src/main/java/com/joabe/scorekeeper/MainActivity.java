afd37472d2f375618cfbfd24931afb0bc3233b55
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-04 09:54:16 -0300

    Initializing the TextView elements and score count variables

--- app/src/main/java/com/joabe/scorekeeper/MainActivity.java
+++ app/src/main/java/com/joabe/scorekeeper/MainActivity.java
@@ -3,12 +3,22 @@ package com.joabe.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mScore1;
+    private int mScore2;
+
+    private TextView mScoreText1;
+    private TextView mScoreText2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mScoreText1 = findViewById(R.id.score_1);
+        mScoreText2 = findViewById(R.id.score_2);
     }
 }
