09cfa5b61e9678334d2b8dc767463cad4bef774a
SCOPUS\jtcosta <jtcosta@scopus.com.br>
2019-10-04 10:03:43 -0300

    Implementing the click handlers for the ImageButton elements

--- app/src/main/java/com/joabe/scorekeeper/MainActivity.java
+++ app/src/main/java/com/joabe/scorekeeper/MainActivity.java
@@ -3,6 +3,7 @@ package com.joabe.scorekeeper;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,5 +21,39 @@ public class MainActivity extends AppCompatActivity {
 
         mScoreText1 = findViewById(R.id.score_1);
         mScoreText2 = findViewById(R.id.score_2);
+    }
+
+    /**
+     * Method that handles the onClick of both the decrement buttons
+     * @param view The button view that was clicked
+     */
+    public void decreaseScore(View view) {
+        int viewID = view.getId();
+
+        switch (viewID) {
+            case R.id.decreaseTeam1:
+                mScore1--;
+                mScoreText1.setText(String.valueOf(mScore1));
+            case R.id.decreaseTeam2:
+                mScore2--;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
+    }
+
+    /**
+     * Method that handles the onClick of both the increment buttons
+     * @param view The button view that was clicked
+     */
+    public void increaseScore(View view) {
+        int viewID = view.getId();
+
+        switch (viewID) {
+            case R.id.increaseTeam1:
+                mScore1++;
+                mScoreText1.setText(String.valueOf(mScore1));
+            case R.id.increaseTeam2:
+                mScore2++;
+                mScoreText2.setText(String.valueOf(mScore2));
+        }
     }
 }
